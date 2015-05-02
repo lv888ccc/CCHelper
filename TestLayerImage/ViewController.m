bf1bc5097c666f0b9f6438a5c1a7d9ca083e9f3a
cc <lvchen_2012@163.com>
2015-05-02 14:08:47 +0800

    test

--- TestLayerImage/ViewController.m
+++ TestLayerImage/ViewController.m
@@ -38,6 +38,8 @@
     
     ///测试
     
+    //在测试一下
+    
     UIImage *image = [UIImage imageNamed:@"1.jpg"];
     
     imgScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, 320, 480)];
